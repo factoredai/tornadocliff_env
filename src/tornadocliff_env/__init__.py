cc5f5e29135fbac38112c1d491c0ac94281151c6
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2026-02-12 12:17:22 -0500

    Fix style issue

--- src/tornadocliff_env/__init__.py
+++ src/tornadocliff_env/__init__.py
@@ -2,6 +2,7 @@ from gymnasium.envs.registration import register
 
 from .tornadocliffenv import TornadoCliffEnv  # noqa :
 
+
 register(
     id="factoredai/TornadoCliff-v0",
     entry_point="tornadocliff_env.tornadocliffenv:TornadoCliffEnv",
