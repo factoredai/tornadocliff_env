73b6e224ac537b5e7cd1ef003a7602df0aeec368
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2026-02-12 12:13:08 -0500

    Add registration code

--- src/tornadocliff_env/__init__.py
+++ src/tornadocliff_env/__init__.py
@@ -1,1 +1,7 @@
 from .tornadocliffenv import TornadoCliffEnv  # noqa :
+from gymnasium.envs.registration import register
+
+register(
+    id="factoredai/TornadoCliff-v0",
+    entry_point="tornadocliff_env.tornadocliffenv:TornadoCliffEnv",
+)
